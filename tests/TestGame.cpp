11f82bfd9027f88c9029294b3a60c5423ed3bad5
Alexis <eros67mc@gmail.com>
2024-04-01 20:13:53 +0200

    Test all game

--- tests/TestGame.cpp
+++ tests/TestGame.cpp
@@ -6,6 +6,12 @@
 #include "GameManager.h"
 #include "ClientNetworkInterface.h"
 #include "ServerNetworkInterface.h"
+#include "Logger.h"
+#include "MyPackets/ChangeDeckPacket.h"
+#include "MyPackets/StartGamePacket.h"
+#include "Random.h"
+#include "MyPackets/CardInformationPacket.h"
+#include "MyPackets/TurnPacket.h"
 
 #include <gtest/gtest.h>
 
@@ -36,8 +42,7 @@ inline static float WIDTH = 1920.f;
 class TestServerNetwork final : public ServerNetworkInterface
 {
 public:
-	std::vector<PacketData> PacketsSent; //TODO: Replace last packet sent with this
-	PacketData LastPacketSent {};
+	std::queue<PacketData> PacketsSent;
 	std::queue<PacketData> PacketsToProcess;
 	std::queue<ClientId> DisconnectedClients;
 
@@ -52,7 +57,16 @@ public:
 
 	void SendPacket(Packet* packet, const ClientId& clientId) override
 	{
-		LastPacketSent = { packet, clientId };
+		PacketsSent.push({ packet, clientId });
+	}
+
+	PacketData PopSentPacket()
+	{
+		if (PacketsSent.empty()) return {};
+
+		auto packet = PacketsSent.front();
+		PacketsSent.pop();
+		return packet;
 	}
 
 	ClientId PopDisconnectedClient() override
@@ -64,6 +78,17 @@ public:
 		return client;
 	}
 };
+
+void CompareLobbyInformations(MyPackets::LobbyInformationPacket* lobbyInformationPacket, GameData::Lobby lobby)
+{
+	ASSERT_EQ(lobbyInformationPacket->IsHost, lobby.IsHost);
+	ASSERT_EQ(lobbyInformationPacket->WaitingForOpponent, lobby.WaitingForOpponent);
+	ASSERT_EQ(lobbyInformationPacket->Player1Name.AsString(), lobby.Player1.Name.AsString());
+	ASSERT_EQ(lobbyInformationPacket->Player1Icon, lobby.Player1.IconIndex);
+	ASSERT_EQ(lobbyInformationPacket->Player2Name.AsString(), lobby.Player2.Name.AsString());
+	ASSERT_EQ(lobbyInformationPacket->Player2Icon, lobby.Player2.IconIndex);
+	ASSERT_EQ(lobbyInformationPacket->ChosenDeckType, lobby.DeckType);
+}
 
 TEST(TestGame, CompleteGame)
 {
@@ -96,6 +121,7 @@ TEST(TestGame, CompleteGame)
 	const auto& p2Player = gameManagers[1].GetPlayer();
 
 	const auto updateTime = sf::milliseconds(10000);
+	const auto cardRevealTime = sf::milliseconds(500);
 
 	gameManagers[0].SetUsername(p1Name.AsString());
 	gameManagers[0].SetPlayerIcon(p1Icon);
@@ -107,59 +133,262 @@ TEST(TestGame, CompleteGame)
 	{
 		games[0].SetState(GameState::LOBBY);
 
-		ASSERT_EQ(p1Lobby.IsHost, true);
-		ASSERT_EQ(p1Lobby.WaitingForOpponent, true);
 		ASSERT_EQ(networkClientManagers[0].LastPacketSent->Type, static_cast<char>(MyPackets::MyPacketType::JoinLobby));
 
 		serverNetwork.PacketsToProcess.push({ networkClientManagers[0].LastPacketSent, clientIds[0] });
 
 		server.Update();
 
-		ASSERT_EQ(serverNetwork.LastPacketSent.Packet->Type, static_cast<char>(MyPackets::MyPacketType::LobbyInformation));
+		auto packet = serverNetwork.PopSentPacket();
 
-		networkClientManagers[0].PacketsToProcess.push(serverNetwork.LastPacketSent.Packet);
+		ASSERT_EQ(packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::LobbyInformation));
+
+		networkClientManagers[0].PacketsToProcess.push(packet.Packet->Clone());
 
 		games[0].Update(updateTime, { 0, 0 });
 
-		const auto lobbyInfoPacket = serverNetwork.LastPacketSent.Packet->As<MyPackets::LobbyInformationPacket>();
-
-		ASSERT_EQ(lobbyInfoPacket->IsHost, p1Lobby.IsHost);
-		ASSERT_EQ(lobbyInfoPacket->WaitingForOpponent, p1Lobby.WaitingForOpponent);
-		ASSERT_EQ(lobbyInfoPacket->Player1Name.AsString(), p1Lobby.Player1.Name.AsString());
-		ASSERT_EQ(lobbyInfoPacket->Player1Icon, p1Lobby.Player1.IconIndex);
-		ASSERT_EQ(lobbyInfoPacket->Player2Name.AsString(), p1Lobby.Player2.Name.AsString());
-		ASSERT_EQ(lobbyInfoPacket->Player2Icon, p1Lobby.Player2.IconIndex);
-		ASSERT_EQ(lobbyInfoPacket->ChosenDeckType, p1Lobby.DeckType);
+		CompareLobbyInformations(packet.Packet->As<MyPackets::LobbyInformationPacket>(), p1Lobby);
+		delete packet.Packet;
 	}
 
 	// 1.2 Player 2 joins the lobby
 	{
 		games[1].SetState(GameState::LOBBY);
 
-		ASSERT_EQ(p2Lobby.IsHost, false);
-		ASSERT_EQ(p2Lobby.WaitingForOpponent, false);
 		ASSERT_EQ(networkClientManagers[1].LastPacketSent->Type, static_cast<char>(MyPackets::MyPacketType::JoinLobby));
 
 		serverNetwork.PacketsToProcess.push({ networkClientManagers[1].LastPacketSent, clientIds[1] });
 
 		server.Update();
 
-		//TODO: Replace last packet sent with PacketsSent
+		// Packet is first sent to player 1
+		auto packet = serverNetwork.PopSentPacket();
 
-		ASSERT_EQ(serverNetwork.LastPacketSent.Packet->Type, static_cast<char>(MyPackets::MyPacketType::LobbyInformation));
+		ASSERT_EQ(packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::LobbyInformation));
 
-		networkClientManagers[1].PacketsToProcess.push(serverNetwork.LastPacketSent.Packet);
+		networkClientManagers[0].PacketsToProcess.push(packet.Packet->Clone());
+
+		games[0].Update(updateTime, { 0, 0 });
+
+		CompareLobbyInformations(packet.Packet->As<MyPackets::LobbyInformationPacket>(), p1Lobby);
+		delete packet.Packet;
+
+		// Packet is then sent to player 2
+		packet = serverNetwork.PopSentPacket();
+
+		ASSERT_EQ(packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::LobbyInformation));
+
+		networkClientManagers[1].PacketsToProcess.push(packet.Packet->Clone());
 
 		games[1].Update(updateTime, { 0, 0 });
 
-		const auto lobbyInfoPacket = networkClientManagers[1].LastPacketSent->As<MyPackets::LobbyInformationPacket>();
+		CompareLobbyInformations(packet.Packet->As<MyPackets::LobbyInformationPacket>(), p2Lobby);
+		delete packet.Packet;
+	}
 
-		ASSERT_EQ(lobbyInfoPacket->IsHost, p2Lobby.IsHost);
-		ASSERT_EQ(lobbyInfoPacket->WaitingForOpponent, p2Lobby.WaitingForOpponent);
-		ASSERT_EQ(lobbyInfoPacket->Player1Name.AsString(), p2Lobby.Player1.Name.AsString());
-		ASSERT_EQ(lobbyInfoPacket->Player1Icon, p2Lobby.Player1.IconIndex);
-		ASSERT_EQ(lobbyInfoPacket->Player2Name.AsString(), p2Lobby.Player2.Name.AsString());
-		ASSERT_EQ(lobbyInfoPacket->Player2Icon, p2Lobby.Player2.IconIndex);
-		ASSERT_EQ(lobbyInfoPacket->ChosenDeckType, p2Lobby.DeckType);
+	// 2. Choose a deck and start
+	// 2.1 With player 2 -> check that he cannot start -> server doesn't send a packet
+	{
+		const DeckType deckType = DeckType::Deck3x2;
+
+		serverNetwork.PacketsToProcess.push({ new MyPackets::ChangeDeckPacket(deckType), clientIds[1] });
+		serverNetwork.PacketsToProcess.push({ new MyPackets::StartGamePacket(), clientIds[1] });
+
+		server.Update();
+
+		auto packet = serverNetwork.PopSentPacket();
+
+		ASSERT_EQ(packet.Packet, nullptr);
+	}
+
+	const DeckType deckType = DeckType::Deck6x5;
+	const auto deckSize = 6 * 5;
+
+	// 2.2 With player 1 -> check that he can start
+	{
+		serverNetwork.PacketsToProcess.push({ new MyPackets::ChangeDeckPacket(deckType), clientIds[0] });
+		serverNetwork.PacketsToProcess.push({ new MyPackets::StartGamePacket(), clientIds[0] });
+
+		server.Update();
+
+		auto packet = serverNetwork.PopSentPacket();
+
+		ASSERT_EQ(packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::StartGame));
+
+		networkClientManagers[0].PacketsToProcess.push(packet.Packet->Clone());
+
+		games[0].Update(updateTime, { 0, 0 });
+
+		ASSERT_EQ(p1Game.YourTurn, packet.Packet->As<MyPackets::StartGamePacket>()->YourTurn);
+		delete packet.Packet;
+
+		packet = serverNetwork.PopSentPacket();
+
+		ASSERT_EQ(packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::StartGame));
+
+		networkClientManagers[1].PacketsToProcess.push(packet.Packet->Clone());
+
+		games[1].Update(updateTime, { 0, 0 });
+
+		ASSERT_EQ(p2Game.YourTurn, packet.Packet->As<MyPackets::StartGamePacket>()->YourTurn);
+		ASSERT_EQ(p2Game.DeckType, packet.Packet->As<MyPackets::StartGamePacket>()->ChosenDeckType);
+		delete packet.Packet;
+	}
+
+	// 3. Play the game until the end
+	{
+		int turn = 0;
+		std::vector<CardIconIndex> deck;
+		std::vector<CardIconIndex> foundCardIcons;
+
+		for (int i = 0; i < deckSize / 2; i++)
+		{
+			deck.push_back(UNKNOWN_ICON_INDEX);
+			deck.push_back(UNKNOWN_ICON_INDEX);
+		}
+
+		// Play turns until the end
+		while (foundCardIcons.size() < deck.size() / 2)
+		{
+			LOG("Turn: " << turn++ << " - Found: " << foundCardIcons.size() << " / " << deck.size() / 2);
+
+			auto& currentGame = p1Game.YourTurn ? games[0] : games[1];
+			auto& otherGame = p1Game.YourTurn ? games[1] : games[0];
+			auto& currentPlayerNetwork = p1Game.YourTurn ? networkClientManagers[0] : networkClientManagers[1];
+			auto& otherPlayerNetwork = p1Game.YourTurn ? networkClientManagers[1] : networkClientManagers[0];
+			auto& currentGameManager = p1Game.YourTurn ? gameManagers[0] : gameManagers[1];
+			const auto& currentClientId = p1Game.YourTurn ? clientIds[0] : clientIds[1];
+
+			const auto pairLeft = deck.size() / 2 - foundCardIcons.size();
+
+			// Search for 2 random cards
+			std::vector<CardIndex> leftCards;
+
+			for (int i = 0; i < deck.size(); i++)
+			{
+				if (foundCardIcons.end() == std::find(foundCardIcons.begin(), foundCardIcons.end(), deck[i]))
+				{
+					leftCards.push_back(CardIndex(i));
+				}
+			}
+
+			const auto card1 = leftCards[0];
+			const auto card2 = leftCards[Random::Range(1, leftCards.size() - 1)];
+
+			// Pick the cards
+			{
+				// Check if the player can pick the cards
+				ASSERT_EQ(currentGameManager.ChooseACard(card1), true);
+				ASSERT_EQ(currentGameManager.ChooseACard(card2), true);
+
+				// Send card information packet to the server and check that each player receive the card icon info
+				{
+					serverNetwork.PacketsToProcess.push({ new MyPackets::CardInformationPacket(card1, UNKNOWN_ICON_INDEX), currentClientId });
+					serverNetwork.PacketsToProcess.push({ new MyPackets::CardInformationPacket(card2, UNKNOWN_ICON_INDEX), currentClientId });
+
+					server.Update();
+
+					auto p1Card1Packet = serverNetwork.PopSentPacket();
+					auto p2Card1Packet = serverNetwork.PopSentPacket();
+					auto p1Card2Packet = serverNetwork.PopSentPacket();
+					auto p2Card2Packet = serverNetwork.PopSentPacket();
+
+					ASSERT_EQ(p1Card1Packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::CardInformation));
+					ASSERT_EQ(p2Card1Packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::CardInformation));
+					ASSERT_EQ(p1Card2Packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::CardInformation));
+					ASSERT_EQ(p2Card2Packet.Packet->Type, static_cast<char>(MyPackets::MyPacketType::CardInformation));
+
+					currentPlayerNetwork.PacketsToProcess.push(p1Card1Packet.Packet->Clone());
+					currentPlayerNetwork.PacketsToProcess.push(p1Card2Packet.Packet->Clone());
+					otherPlayerNetwork.PacketsToProcess.push(p2Card1Packet.Packet->Clone());
+					otherPlayerNetwork.PacketsToProcess.push(p2Card2Packet.Packet->Clone());
+
+					// Double update for the animations
+					currentGame.Update(updateTime, { 0, 0 });
+					currentGame.Update(cardRevealTime, { 0, 0 });
+					otherGame.Update(updateTime, { 0, 0 });
+					otherGame.Update(cardRevealTime, { 0, 0 });
+
+					const auto& p1Card1InformationPacket = p1Card1Packet.Packet->As<MyPackets::CardInformationPacket>();
+					const auto& p2Card1InformationPacket = p2Card1Packet.Packet->As<MyPackets::CardInformationPacket>();
+					const auto& p1Card2InformationPacket = p1Card2Packet.Packet->As<MyPackets::CardInformationPacket>();
+					const auto& p2Card2InformationPacket = p2Card2Packet.Packet->As<MyPackets::CardInformationPacket>();
+
+					ASSERT_EQ(p1Card1InformationPacket->CardIndexInDeck, card1);
+					ASSERT_EQ(p2Card1InformationPacket->CardIndexInDeck, card1);
+					ASSERT_EQ(p1Card2InformationPacket->CardIndexInDeck, card2);
+					ASSERT_EQ(p2Card2InformationPacket->CardIndexInDeck, card2);
+
+					ASSERT_NE(p1Card1InformationPacket->IconIndex, UNKNOWN_ICON_INDEX);
+					ASSERT_NE(p2Card1InformationPacket->IconIndex, UNKNOWN_ICON_INDEX);
+					ASSERT_NE(p1Card2InformationPacket->IconIndex, UNKNOWN_ICON_INDEX);
+					ASSERT_NE(p2Card2InformationPacket->IconIndex, UNKNOWN_ICON_INDEX);
+
+
+					ASSERT_EQ(p1Game.CardIndex1, card1);
+					ASSERT_EQ(p2Game.CardIndex1, card1);
+					ASSERT_EQ(p1Game.CardIndex2, card2);
+					ASSERT_EQ(p2Game.CardIndex2, card2);
+
+					deck[card1.Index] = p1Card1InformationPacket->IconIndex;
+					deck[card2.Index] = p1Card2InformationPacket->IconIndex;
+
+					if (deck[card1.Index] == deck[card2.Index])
+					{
+						foundCardIcons.push_back(deck[card1.Index]);
+					}
+
+					delete p1Card1Packet.Packet;
+					delete p2Card1Packet.Packet;
+					delete p1Card2Packet.Packet;
+					delete p2Card2Packet.Packet;
+				}
+			}
+
+			// Check turn packet
+			if (pairLeft > 1)
+			{
+				const auto p1Turn = p1Game.YourTurn;
+				const auto p2Turn = p2Game.YourTurn;
+
+				auto p1TurnPacket = serverNetwork.PopSentPacket();
+				auto p2TurnPacket = serverNetwork.PopSentPacket();
+
+				ASSERT_EQ(p1TurnPacket.Packet->Type, static_cast<char>(MyPackets::MyPacketType::Turn));
+				ASSERT_EQ(p2TurnPacket.Packet->Type, static_cast<char>(MyPackets::MyPacketType::Turn));
+
+				networkClientManagers[0].PacketsToProcess.push(p1TurnPacket.Packet->Clone());
+				networkClientManagers[1].PacketsToProcess.push(p2TurnPacket.Packet->Clone());
+
+				games[0].Update(updateTime, { 0, 0 });
+				games[0].Update(updateTime, { 0, 0 });
+				games[1].Update(updateTime, { 0, 0 });
+				games[1].Update(updateTime, { 0, 0 });
+
+				const auto& p1TurnPacketData = p1TurnPacket.Packet->As<MyPackets::TurnPacket>();
+				const auto& p2TurnPacketData = p2TurnPacket.Packet->As<MyPackets::TurnPacket>();
+
+				ASSERT_EQ(p1Game.YourTurn, p1TurnPacketData->YourTurn);
+				ASSERT_EQ(p2Game.YourTurn, p2TurnPacketData->YourTurn);
+
+				if (deck[card1.Index] == deck[card2.Index])
+				{
+					ASSERT_EQ(p1Game.YourTurn, p1Turn);
+					ASSERT_EQ(p2Game.YourTurn, p2Turn);
+
+					LOG("Found a pair");
+				}
+				else
+				{
+					ASSERT_NE(p1Game.YourTurn, p1Turn);
+					ASSERT_NE(p2Game.YourTurn, p2Turn);
+				}
+
+				delete p1TurnPacket.Packet;
+				delete p2TurnPacket.Packet;
+			}
+		}
 	}
 }
+
+//TODO: Test lobbies
